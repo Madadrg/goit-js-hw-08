a1c5796a6cfc8d5bf35c440b4f07ee11d499fe20
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-02-03 20:01:43 +0200

    Solved exercises

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Vimeo from 'vimeo-player';
+import throttle from 'lodash.throttle';
 
+const playerElement = document.getElementById('vimeo-player');
+
+const player = new Vimeo(playerElement);
+
+const savePlaybackTime = time => {
+  localStorage.setItem('videoplayer-current-time', time);
+};
+
+const loadPlaybackTime = () => {
+  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+};
+
+player.setCurrentTime(loadPlaybackTime());
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    savePlaybackTime(data.seconds);
+  }, 1000)
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,39 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = form.querySelector('input[name="email"]');
+const messageInput = form.querySelector('textarea[name="message"]');
+
+const saveFormState = throttle(() => {
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(formState));
+}, 500);
+
+const loadFormState = () => {
+  const storedState = localStorage.getItem('feedback-form-state');
+  if (storedState) {
+    const { email, message } = JSON.parse(storedState);
+    emailInput.value = email;
+    messageInput.value = message;
+  }
+};
+
+const handleSubmit = event => {
+  event.preventDefault();
+  const formState = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  console.log('Form submitted with data:', formState);
+
+  localStorage.removeItem('feedback-form-state');
+};
+
+emailInput.addEventListener('input', saveFormState);
+messageInput.addEventListener('input', saveFormState);
+form.addEventListener('submit', handleSubmit);
+
+loadFormState();
