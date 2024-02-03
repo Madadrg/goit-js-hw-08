431b7506a50aae86503f46906916b0518aa8d0a8
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-02-03 20:17:33 +0200

    Changed code for second exercise

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,23 +1,32 @@
-import Vimeo from 'vimeo-player';
+import VimeoPlayer from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-const playerElement = document.getElementById('vimeo-player');
+const vimeoPlayer = new VimeoPlayer(document.getElementById('vimeo-player'));
 
-const player = new Vimeo(playerElement);
-
+// Function to save playback time to local storage
 const savePlaybackTime = time => {
   localStorage.setItem('videoplayer-current-time', time);
 };
 
+// Function to load and set playback time from local storage
 const loadPlaybackTime = () => {
-  return parseFloat(localStorage.getItem('videoplayer-current-time')) || 0;
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    vimeoPlayer.setCurrentTime(parseFloat(savedTime));
+  }
 };
 
-player.setCurrentTime(loadPlaybackTime());
+// Initialize the player
+vimeoPlayer.ready().then(() => {
+  // Load playback time from local storage on player ready
+  loadPlaybackTime();
 
-player.on(
-  'timeupdate',
-  throttle(data => {
-    savePlaybackTime(data.seconds);
-  }, 1000)
-);
+  // Add event listener for timeupdate using lodash.throttle
+  vimeoPlayer.on(
+    'timeupdate',
+    throttle(event => {
+      // Save playback time to local storage
+      savePlaybackTime(event.seconds);
+    }, 1000)
+  ); // Throttle to once per second
+});
